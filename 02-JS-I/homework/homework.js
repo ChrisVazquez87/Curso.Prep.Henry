5fd27532d96d2bc5969f726eda241829d8bd9cb1
ChrisVazquez87 <90289226+ChrisVazquez87@users.noreply.github.com>
2021-10-12 18:56:06 -0500

    actualizar jsII

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -7,7 +7,7 @@ const nuevaString = 'chris';
 const nuevoNum = 87;
 
 // Crea una variable booleana:
-const nuevoBool = true;
+const nuevoBool = false;
 
 // Resuelve el siguiente problema matemático:
 const nuevaResta = 10 - 5 === 5;
